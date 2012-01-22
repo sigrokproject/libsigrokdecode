d567790b9c0ab30b18aceb92e6714e631200e9b9
Uwe Hermann <uwe@hermann-uwe.de>
2012-01-22 14:28:48 +0100

    srd: Add generic i2cdemux protocol decoder.

---
+++ decoders/i2cdemux/i2cdemux.py
@@ -0,0 +1,90 @@
+##
+## This file is part of the sigrok project.
+##
+## Copyright (C) 2012 Uwe Hermann <uwe@hermann-uwe.de>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+##
+
+#
+# Generic I2C demultiplexing protocol decoder
+#
+# Takes an I2C stream as input and outputs multiple I2C streams, each stream
+# containing only I2C packets for one specific I2C slave.
+#
+
+import sigrokdecode as srd
+
+class Decoder(srd.Decoder):
+    api_version = 1
+    id = 'i2cdemux'
+    name = 'I2C demux'
+    longname = 'Generic I2C demultiplexer'
+    desc = 'TODO.'
+    longdesc = 'TODO.'
+    license = 'gplv2+'
+    inputs = ['i2c']
+    outputs = [] # TODO: Only known at run-time.
+    probes = []
+    options = {}
+    annotations = []
+
+    def __init__(self, **kwargs):
+        self.packets = [] # Local cache of I2C packets
+        self.slaves = [] # List of known slave addresses
+        self.stream = -1 # Current output stream
+        self.streamcount = 0 # Number of created output streams
+
+    def start(self, metadata):
+        self.out_proto = []
+
+    def report(self):
+        pass
+
+    # Grab I2C packets into a local cache, until an I2C STOP condition
+    # packet comes along. At some point before that STOP condition, there
+    # will have been an ADDRESS READ or ADDRESS WRITE which contains the
+    # I2C address of the slave that the master wants to talk to.
+    # We use this slave address to figure out which output stream should
+    # get the whole chunk of packets (from START to STOP).
+    def decode(self, ss, es, data):
+
+        cmd, databyte, ack = data
+
+        # Add the I2C packet to our local cache.
+        self.packets += [[ss, es, data]]
+
+        if cmd in ('ADDRESS READ', 'ADDRESS WRITE'):
+            if databyte in self.slaves:
+                self.stream = self.slaves.index(databyte)
+                return
+
+            # We're never seen this slave, add a new stream.
+            self.slaves += [databyte]
+            self.out_proto += [self.add(srd.OUTPUT_PROTO,
+                               'i2c-%s' % hex(databyte))]
+            self.stream = self.streamcount
+            self.streamcount += 1
+        elif cmd == 'STOP':
+            if self.stream == -1:
+                raise Exception('Invalid stream!') # FIXME?
+
+            # Send the whole chunk of I2C packets to the correct stream.
+            for p in self.packets:
+                self.put(p[0], p[1], self.out_proto[self.stream], p[2])
+
+            self.packets = []
+            self.stream = -1
+
