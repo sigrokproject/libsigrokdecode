77adad943111ab064f01838033dc9c94152f46c7
Uwe Hermann <uwe@hermann-uwe.de>
2012-07-28 10:51:32 +0200

    srd: avr_isp: Factor out part numbers/names to part.py.

--- decoders/avr_isp/parts.py
+++ decoders/avr_isp/parts.py
@@ -18,9 +18,26 @@
 ## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 ##
 
-pkgdatadir = $(DECODERS_DIR)/avr_isp
+# Device code addresses:
+# 0x00: vendor code, 0x01: part family + flash size, 0x02: part number
 
-dist_pkgdata_DATA = __init__.py pd.py parts.py
+# Vendor code
+vendor_code = {
+    0x1e: 'Atmel',
+    0x00: 'Device locked',
+}
 
-CLEANFILES = *.pyc
+# (Part family + flash size, part number)
+part = {
+    (0x90, 0x01): 'AT90S1200',
+    (0x91, 0x01): 'AT90S2313',
+    (0x92, 0x01): 'AT90S4414',
+    (0x92, 0x05): 'ATmega48', # 4kB flash
+    (0x93, 0x01): 'AT90S8515',
+    (0x93, 0x0a): 'ATmega88', # 8kB flash
+    (0x94, 0x06): 'ATmega168', # 16kB flash
+    (0xff, 0xff): 'Device code erased, or target missing',
+    (0x01, 0x02): 'Device locked',
+    # TODO: Lots more entries.
+}
 
--- decoders/avr_isp/pd.py
+++ decoders/avr_isp/pd.py
@@ -21,29 +21,7 @@
 # AVR ISP protocol decoder
 
 import sigrokdecode as srd
-
-# Device code addresses:
-# 0x00: vendor code, 0x01: part family + flash size, 0x02: part number
-
-# Vendor code
-vendor_code = {
-    0x1e: 'Atmel',
-    0x00: 'Device locked',
-}
-
-# (Part family + flash size, part number)
-part = {
-    (0x90, 0x01): 'AT90S1200',
-    (0x91, 0x01): 'AT90S2313',
-    (0x92, 0x01): 'AT90S4414',
-    (0x92, 0x05): 'ATmega48', # 4kB flash
-    (0x93, 0x01): 'AT90S8515',
-    (0x93, 0x0a): 'ATmega88', # 8kB flash
-    (0x94, 0x06): 'ATmega168', # 16kB flash
-    (0xff, 0xff): 'Device code erased, or target missing',
-    (0x01, 0x02): 'Device locked',
-    # TODO: Lots more entries.
-}
+from .parts import *
 
 VENDOR_CODE_ATMEL = 0x1e
 
