83df730dd21ce52670a593455cfe5e732989a90d
Uwe Hermann <uwe@hermann-uwe.de>
2014-07-31 00:02:22 +0200

    tests/check_session.c: Fix compiler warning.
    This fixes 'warning: "_POSIX_C_SOURCE" redefined' with some Python
    versions.

--- tests/check_session.c
+++ tests/check_session.c
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#include "../libsigrokdecode.h" /* First, to avoid compiler warning. */
-#include "../libsigrokdecode-internal.h"
+#include "../libsigrokdecode-internal.h" /* First, to avoid compiler warning. */
+#include "../libsigrokdecode.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <check.h>
