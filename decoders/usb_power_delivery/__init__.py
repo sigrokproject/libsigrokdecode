fddb311472d4458382418de3f091bd1b997d3a89
Uwe Hermann <uwe@hermann-uwe.de>
2015-11-21 19:35:15 +0100

    usb_power_delivery: Use the same license header format as other PDs.

--- decoders/usb_power_delivery/__init__.py
+++ decoders/usb_power_delivery/__init__.py
@@ -1,7 +1,7 @@
 ##
 ## This file is part of the libsigrokdecode project.
 ##
-## Copyright 2015 Google, Inc
+## Copyright (C) 2015 Google, Inc
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -12,6 +12,10 @@
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 ##
 
 """
