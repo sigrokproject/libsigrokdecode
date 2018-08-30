2842721f189c5338d268a6e70002936289ae6069
Uwe Hermann <uwe@hermann-uwe.de>
2018-08-30 20:42:05 +0200

    srd_decoder_unload_all(): Fix a -Wcast-function-type compiler warning.
      decoder.c: In function ‘srd_decoder_unload_all’:
      decoder.c:1080:27: warning: cast between incompatible function types from ‘int (*)(struct srd_decoder *)’ to ‘void (*)(void *, void *)’ [-Wcast-function-type]
        g_slist_foreach(pd_list, (GFunc)srd_decoder_unload, NULL);
                               ^

--- decoder.c
+++ decoder.c
@@ -1077,7 +1077,8 @@ SRD_API int srd_decoder_load_all(void)
  */
 SRD_API int srd_decoder_unload_all(void)
 {
-	g_slist_foreach(pd_list, (GFunc)srd_decoder_unload, NULL);
+	for (GSList *l = pd_list; l; l = l->next)
+		srd_decoder_unload(l->data);
 	g_slist_free(pd_list);
 	pd_list = NULL;
 
