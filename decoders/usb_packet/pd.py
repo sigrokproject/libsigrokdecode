f78b814d54b19233d5e3f7cfef3d30a7a5ac8d40
Ryan Jarvis <ryan@shopboxretail.com>
2019-01-18 23:51:23 -0800

    Remove trailing semicolons

--- decoders/usb_packet/pd.py
+++ decoders/usb_packet/pd.py
@@ -147,7 +147,7 @@ def reverse_number(num, count):
     out = list(count * '0')
     for i in range(0, count):
         if num >> i & 1:
-            out[i] = '1';
+            out[i] = '1'
     return int(''.join(out), 2)
 
 def calc_crc5(bitstr):
