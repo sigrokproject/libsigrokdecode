eee48b666e350ff05548f12c8244fb8b609d3a86
Uwe Hermann <uwe@hermann-uwe.de>
2019-12-07 17:29:14 +0100

    hdcp: Add missing spaces.

--- decoders/hdcp/__init__.py
+++ decoders/hdcp/__init__.py
@@ -1,7 +1,7 @@
 ##
 ## This file is part of the libsigrokdecode project.
 ##
-## Copyright (C) 2018 Dave Craig<dcraig@brightsign.biz>
+## Copyright (C) 2018 Dave Craig <dcraig@brightsign.biz>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
