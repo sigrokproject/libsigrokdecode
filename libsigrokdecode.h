2ee740fd9e5197bc9951a0c92e35947203c1b620
Soeren Apel <soeren@apelpie.net>
2020-06-26 20:18:35 +0200

    Use uint8_t instead of unsigned char for raw byte data

--- libsigrokdecode.h
+++ libsigrokdecode.h
@@ -323,12 +323,12 @@ struct srd_proto_data_annotation {
 struct srd_proto_data_binary {
 	int bin_class; /* Index into "struct srd_decoder"->binary. */
 	uint64_t size;
-	const unsigned char *data;
+	const uint8_t *data;
 };
 struct srd_proto_data_logic {
 	int logic_group;
 	uint64_t repeat_count; /* Number of times the value in data was repeated. */
-	const unsigned char *data; /* Bitfield containing the states of the logic outputs */
+	const uint8_t *data; /* Bitfield containing the states of the logic outputs */
 };
 
 typedef void (*srd_pd_output_callback)(struct srd_proto_data *pdata,
