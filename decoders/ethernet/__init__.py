99bdaf444984b81f5e00a11891d8d1ba7e3213d6
Sam <sam210723@hotmail.com>
2021-02-28 21:12:12 +1000

    Update Ethernet description

--- decoders/ethernet/__init__.py
+++ decoders/ethernet/__init__.py
@@ -1,5 +1,10 @@
 '''
-An Ethernet frame is a data link layer protocol data unit which uses the underlying Ethernet physical layer transport mechanisms.
+Ethernet is a data link layer protocol found in Local/Wide Area Networks (LAN/WAN) typically carrying the Internet Protocol (IPv4 / IPv6).
+It is considered one of the key technologies that make up the Internet.
+
+Systems communicating over Ethernet divide a stream of data into shorter pieces called frames. Each frame contains source and destination addresses, and error-checking data so that damaged frames can be detected and discarded.
+
+This decoder stacks on top of the '4B5B' PD and decodes bytes into Ethernet frames.
 '''
 
 from .pd import Decoder
