be0442c9f01e5971d1121c47227e522ed4b28fb5
Marshal Horn <kamocat@gmail.com>
2023-04-28 15:32:34 -0700

    ps2: Added mouse and keyboard stacked decoders
    Mouse decoder interprets the 3-byte packets for movement and clicking, but
    ignores the configuration commands from the host.
    Keyboard decoder interprets press and release messages for each key but does not keep track of state (capslock, shift, etc).

---
+++ decoders/ps2_keyboard/__init__.py
@@ -0,0 +1,25 @@
+##
+## This file is part of the libsigrokdecode project.
+##
+## Copyright (C) 2023 Marshal Horn <kamocat@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+'''
+This protocol decoder can decode PS/2 keyboard commands.
+It should be stacked on the PS/2 packet decoder.
+'''
+
+from .pd import Decoder
